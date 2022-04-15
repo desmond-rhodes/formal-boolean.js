3e3a8571a381a5dcf5bfeb1d433df88acce04562
Desmond Rhodes <desmond.rhodes@outlook.com>
2022-04-16 01:55:18 +0700

    Output mechanism

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ expression.addEventListener("keydown", enter);
 const button = document.getElementById("calculate");
 button.addEventListener("click", calculate);
 
+const template = document.getElementById("output");
+
 function calculate() {
-	console.log(expression.value);
+	const output = template.cloneNode(true);
+
+	const message = document.createElement("p");
+	message.innerHTML = expression.value;
+
+	output.appendChild(message);
+
+	const replace = document.getElementById("output");
+	replace.parentNode.replaceChild(output, replace);
 }
 
 function enter(event) {
